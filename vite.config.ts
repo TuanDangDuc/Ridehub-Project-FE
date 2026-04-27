fbe573e505bca240feb2ef2843ba1f9538af326f
TuanDangDuc <tdang9078@gmail.com>
2026-04-27 20:54:22 +0700

    fix vite conf

--- vite.config.ts
+++ vite.config.ts
@@ -1,16 +1,29 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import tailwindcss from "@tailwindcss/vite";
+import react from "@vitejs/plugin-react";
+import path from "path";
+import { defineConfig, loadEnv } from "vite";
 
-// https://vite.dev/config/
-export default defineConfig({
-  plugins: [react()],
-  server: {
-    proxy: {
-      '/api': {
-        target: 'https://api.anhchuno.id.vn',
-        changeOrigin: true,
-        secure: false,
-      }
-    }
-  }
-})
+export default defineConfig(({ mode }) => {
+  const env = loadEnv(mode, ".", "");
+  return {
+    plugins: [react(), tailwindcss()],
+    define: {
+      "process.env.GEMINI_API_KEY": JSON.stringify(env.GEMINI_API_KEY),
+    },
+    resolve: {
+      alias: {
+        "@": path.resolve(__dirname, "."),
+      },
+    },
+    server: {
+      hmr: process.env.DISABLE_HMR !== "true",
+      proxy: {
+        "/api": {
+          target: "https://api.anhchuno.id.vn/",
+          changeOrigin: true,
+          secure: false,
+        },
+      },
+    },
+  };
+});
